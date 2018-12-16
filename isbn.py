f1736b8c26290ced1957465985a8ca6fa9906c7b
Aaron Robson <AaronRobson@users.noreply.github.com>
2018-12-16 02:12:48 +0000

    Fix flake8 style

--- isbn.py
+++ isbn.py
@@ -5,8 +5,9 @@ import string
 ten = 'X'
 
 ISBN_DET = {}
-ISBN_DET[10] = {'mul': list(range(10, 0, -1)), 'mod': 11} # "range(10,0,-1)" or "range(10,1)[::-1]" suffices for "range(1,10,-1)"
+ISBN_DET[10] = {'mul': list(range(10, 0, -1)), 'mod': 11}
 ISBN_DET[13] = {'mul': [1, 3], 'mod': 10}
+
 
 def Multiply(x, y):
     if x in ten:
@@ -14,21 +15,22 @@ def Multiply(x, y):
     else:
         return int(x)*int(y)
 
+
 def ExpandMul(seq, leng):
-    #while len(outlist) <= leng: outlist += list # <= means it goes 1 repetition too far ie too many mul list items for the input list items
     outlist = seq * ((leng // len(seq)) + 1)
 
-    #reduce the amount by one until the amount is exactly right otherwise the map function this is used in breaks
+    # reduce the amount by one until the amount is exactly right otherwise
+    # the map function this is used in breaks
     while leng < len(outlist):
         outlist = outlist[:-1]
 
     return outlist
 
-def IsbnCheck(isbn, giveChecksum=False, caseSensitive=False):
-    '''Autodetection of type of ISBN system being used (via length). Default to checking a complete ISBN.
-    '''
-    #print('isbncheck - isbn: %r, giveChecksum: %s, caseSensitive: %s.' % (isbn, bool(giveChecksum), bool(caseSensitive)))
 
+def check(isbn, giveChecksum=False, caseSensitive=False):
+    '''Autodetection of type of ISBN system being used (via length).
+    Default to checking a complete ISBN.
+    '''
     isbn = isbn.strip()
 
     if not caseSensitive:
@@ -42,27 +44,24 @@ def IsbnCheck(isbn, giveChecksum=False, caseSensitive=False):
         if ten in isbn[:-1]:
             raise ValueError('X may only be at the end')
 
-        # remove any x's from all but the last character of isbn
-        #isbn = ''.join([item for item in isbn[:-1] if item in string.digits] + [isbn[-1:]])
-
     length = len(isbn)
     if giveChecksum:
         length += 1
 
-    if length not in ISBN_DET: # if the particular length of ISBN is defined above in ISBN_DET then process it, otherwise raise exception
+    # if the particular length of ISBN is defined above in ISBN_DET then
+    # process it, otherwise raise exception
+    if length not in ISBN_DET:
         raise ValueError('Invalid length')
     else:
-        mul = ExpandMul(ISBN_DET[length]['mul'], len(isbn)) # cause the mul to be continuously repeated until it is >= to the isbn in terms of the length
+        # cause the mul to be continuously repeated until it is >= to the
+        # isbn in terms of the length
+        mul = ExpandMul(ISBN_DET[length]['mul'], len(isbn))
         mod = ISBN_DET[length]['mod']
 
         totalList = map(Multiply, isbn, mul)
         total = 0
         for n in totalList:
             total += n
-
-        #print isbn
-        #print 'length of isbn ' + str(len(isbn))
-        #print 'length of mul ' + str(len(mul))
 
         if giveChecksum:
             checkSum = mod - (total % mod)
@@ -73,26 +72,31 @@ def IsbnCheck(isbn, giveChecksum=False, caseSensitive=False):
         else:
             return total % mod == 0
 
+
 def CommandLine(args):
-    args = args[1:] #strip off the script filename (in the 0 position)
+    # strip off the script filename (in the 0 position)
+    args = args[1:]
 
     if len(args):
-        type = False #to default to checking complete ISBNs if not specified otherwise
+        # to default to checking complete ISBNs if not specified otherwise
+        type = False
         for arg in args:
             if arg in ('-h', '--help'):
-                print ('Help: This program checks ISBN numbers of 10 and 13 lengths and calculates check digits')
+                print(
+                    'Help: This program checks ISBN numbers of 10 and 13' +
+                    'lengths and calculates check digits')
             elif arg in ('-t', '--test'):
-                ResultDisplay('ISBN0-596-00281-5') #True
-                ResultDisplay('ISBN0-596-00281-', True) #5
+                ResultDisplay('ISBN0-596-00281-5')  # True
+                ResultDisplay('ISBN0-596-00281-', True)  # 5
 
-                ResultDisplay('978-0-306-40615-7') #True
-                ResultDisplay('978-0-306-40615-', True) #7
+                ResultDisplay('978-0-306-40615-7')  # True
+                ResultDisplay('978-0-306-40615-', True)  # 7
 
-                ResultDisplay('1903397-26-x') #True
-                ResultDisplay('1903397-26-', True) #X
+                ResultDisplay('1903397-26-x')  # True
+                ResultDisplay('1903397-26-', True)  # X
 
-                ResultDisplay('0-313-20060-2') #True
-                ResultDisplay('0-313-20060-', True) #2
+                ResultDisplay('0-313-20060-2')  # True
+                ResultDisplay('0-313-20060-', True)  # 2
             elif arg in ('-s', '--sum'):
                 type = True
             elif arg in ('-c', '--check'):
@@ -100,14 +104,17 @@ def CommandLine(args):
             else:
                 ResultDisplay(arg, type)
     else:
-        print ('ISBN number checker\n')
-        while (True):
-            isbn = input('Enter an ISBN Number (leave blank to quit): ').strip()
+        print('ISBN number checker\n')
+        while True:
+            isbn = input(
+                'Enter an ISBN Number (leave blank to quit): ').strip()
             if not isbn:
                 break
 
             while (True):
-                checkSumStr = input('Type 0 (default) for Check or 1 for Checksum generation: ').strip()
+                checkSumStr = input(
+                    'Type 0 (default) for Check' +
+                    'or 1 for Checksum generation: ').strip()
                 if checkSumStr:
                     try:
                         giveCheckSum = int(checkSumStr)
@@ -121,12 +128,14 @@ def CommandLine(args):
 
             ResultDisplay(isbn, giveCheckSum)
 
+
 def ResultDisplay(isbn, giveCheckSum=None):
     try:
-        print (IsbnCheck(isbn, giveCheckSum))
+        print(check(isbn, giveCheckSum))
     except ValueError as e:
         print(e)
-    print ('')
+    print()
+
 
 if __name__ == '__main__':
     import sys
--- test_isbn.py
+++ test_isbn.py
@@ -2,23 +2,23 @@
 
 import unittest
 
-import isbn
+from isbn import check
 
 
 class TestISBN(unittest.TestCase):
 
     def test(self):
-        self.assertTrue(isbn.IsbnCheck('ISBN 0-596-00281-5'))
-        self.assertEqual(isbn.IsbnCheck('ISBN 0-596-00281-', giveChecksum=True), 5)
+        self.assertTrue(check('ISBN 0-596-00281-5'))
+        self.assertEqual(check('ISBN 0-596-00281-', giveChecksum=True), 5)
 
-        self.assertTrue(isbn.IsbnCheck('978-0-306-40615-7'))
-        self.assertEqual(isbn.IsbnCheck('978-0-306-40615-', giveChecksum=True), 7)
+        self.assertTrue(check('978-0-306-40615-7'))
+        self.assertEqual(check('978-0-306-40615-', giveChecksum=True), 7)
 
-        self.assertTrue(isbn.IsbnCheck('1903397-26-x'))
-        self.assertEqual(isbn.IsbnCheck('1903397-26-', giveChecksum=True), 'X')
+        self.assertTrue(check('1903397-26-x'))
+        self.assertEqual(check('1903397-26-', giveChecksum=True), 'X')
 
-        self.assertTrue(isbn.IsbnCheck('0-313-20060-2'))
-        self.assertEqual(isbn.IsbnCheck('0-313-20060-', giveChecksum=True), 2)
+        self.assertTrue(check('0-313-20060-2'))
+        self.assertEqual(check('0-313-20060-', giveChecksum=True), 2)
 
 
 if __name__ == "__main__":
